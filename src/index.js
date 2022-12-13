0c8fa99e251a17889e23b77c38009a44b25584c7
umair-rao <umairmuqeem@gmail.com>
2022-12-13 19:51:30 +0500

    Remove linters Error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import TodoContainer from './components/TodoContainer';
 import './App.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-<Router basename={process.env.PUBLIC_URL}>
-  <TodoContainer />
-</Router>
+  <Router basename={process.env.PUBLIC_URL}>
+    <TodoContainer />
+  </Router>,
 );
