8f3540e218eeaa626f0b31238b22e0e85fd78b42
umair-rao <umairmuqeem@gmail.com>
2022-12-13 19:14:15 +0500

    Web deployment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import TodoContainer from './components/TodoContainer';
 import './App.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <TodoContainer />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Router>
+  <TodoContainer />
+</Router>,
 );
